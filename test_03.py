45759588696ca52e2ca39712ddcab9c323efba6c
Benazir Ali <ben99first@gmail.com>
2022-02-23 19:30:46 +0300

    Wrote test 03

---
+++ test_03.py
@@ -0,0 +1,12 @@
+import pytest
+import logging
+
+logging.basicConfig(filename="test_01.log", level=logging.DEBUG)
+log = logging.getLogger()
+
+
+class Test03Android:
+    @pytest.mark.skip(reason="Unable to execute test")
+    def test_03(self):
+        log.info("This test is to be skipped")
+
